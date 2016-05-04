088957c4e20cec2cf68655b957bca4315524fce3
Bruemor <arymer1@gmail.com>
2016-05-04 17:33:04 -0400

    Re worked what the project page would look like. Realized that the write up was too much and was taking up too much area on my native screen. It is now more of a path controller page where you can choose what project you want to look into. Once the user clicks the project tile they will be navigated to that projects detail page.

--- app/scripts/components/projects.jsx
+++ app/scripts/components/projects.jsx
@@ -16,19 +16,58 @@ var ProjectsComponent = React.createClass({
         <div className="row">
           <div className="project-one-container col-md-12">
 
-            <div className="kindred-writeup"></div>
-
-            <div className="kindred-photo-slideshow">
-              <img src="images/seaisle-signage.jpg" />
+            <div className="kindred-writeup col-md-4">
+              <div className="transparent-background">
+                <h3>Kindred</h3>
+                <p>Osceola County, Fl</p>
+                <p>Project type: Single Family Community Development</p>
+                <p>Size: 1,450 Acres, 3,000 Units</p>
+              </div>
             </div>
 
-          </div>
-          <div className="project-two-container col-md-12">
+            <div className="waterleigh-landing col-md-4">
+              <div className="transparent-background">
+                <h3>Waterleigh</h3>
+                <p>location here</p>
+                <p>Project Type: Single Family Community Development</p>
+                <p>Size: values here</p>
+              </div>
+            </div>
 
-            <div className="waterleigh-photo-slideshow"></div>
+            <div className="nonapark-landing col-md-4">
+              <div className="transparent-background">
+                <h3>Nona Park</h3>
+                <p>location here</p>
+                <p>Project Type: type here</p>
+                <p>Size: values here</p>
+              </div>
+            </div>
 
-            <div className="waterleigh-writeup">
+            <div className="seaisle-landing col-md-4">
+              <div className="transparent-background">
+                <h3>Sea Isle</h3>
+                <p>location here</p>
+                <p>Project Type: type here</p>
+                <p>Size: values here</p>
+              </div>
+            </div>
 
+            <div className="altis-landing col-md-4">
+              <div className="transparent-background">
+                <h3>Altis at Sandlake</h3>
+                <p>location here</p>
+                <p>Project Type: type here</p>
+                <p>Size: values here</p>
+              </div>
+            </div>
+
+            <div className="dwell-landing col-md-4">
+              <div className="transparent-background">
+                <h3>Dwell at Nona Place</h3>
+                <p>location here</p>
+                <p>Project Type: type here</p>
+                <p>Size: values here</p>
+              </div>
             </div>
 
           </div>
