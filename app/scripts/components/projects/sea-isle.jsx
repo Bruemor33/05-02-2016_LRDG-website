9059d1a44e49c7ba9f01b92a3c1a8a1f12f37349
Bruemor <arymer1@gmail.com>
2016-05-05 14:57:56 -0400

    Made a projects folder to orginized the specific components for each project

---
+++ app/scripts/components/projects/sea-isle.jsx
@@ -0,0 +1,35 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('Backbone-React-Component');
+
+
+
+var SeaIsleComponent = React.createClass({
+
+
+  render: function(){
+
+    return(
+      <div id="sea-isle-background" className="container-fluid">
+        <div classNme="col-md-12">
+          <div className="sea-isle-writeup">
+            <h2>Sea Isle</h2>
+            <p>
+              Located within the Sea World Orlando Design Corridor, Sea Isle is a coastal inspired, multi-family community with sand, landscape, bold colored accents, and rich textures used to create a unique, resort-style sense of place.  Amenities near the clubhouse were designed on axis with different clubhouse architectural elements to maximize space efficacy, views, and use.  In addition to the 356 units, the design program includes a resort style swimming pool, dog park, outdoor fireplace, grill stations, dining area, gated entrance feature, custom columns, decorative paving, and lush landscaping.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+
+
+//Exports
+module.exports = {
+  'SeaIsleComponent': SeaIsleComponent
+}
