efdc88ee396a276a87b522ebf372688700f2ea65
Bruemor <arymer1@gmail.com>
2016-05-05 13:31:28 -0400

    made the project options clickable links to lead to their respectfull pages.

--- app/scripts/components/projects.jsx
+++ app/scripts/components/projects.jsx
@@ -17,57 +17,63 @@ var ProjectsComponent = React.createClass({
           <div className="project-one-container col-md-12">
 
             <div className="kindred-writeup col-md-4">
-              <div className="transparent-background">
-                <h3>Kindred</h3>
-                <p>Osceola County, Fl</p>
-                <p>Project type: Single Family Community Development</p>
-                <p>Size: 1,450 Acres, 3,000 Units</p>
-              </div>
+              <a href={"#kindred"}>
+                <div className="transparent-background">
+                  <h3>Kindred</h3>
+                  <p>Osceola County, Fl</p>
+                  <p>Size: 1,450 Acres, 3,000 Units</p>
+                </div>
+              </a>
             </div>
 
             <div className="waterleigh-landing col-md-4">
-              <div className="transparent-background">
-                <h3>Waterleigh</h3>
-                <p>location here</p>
-                <p>Project Type: Single Family Community Development</p>
-                <p>Size: values here</p>
-              </div>
+              <a href={"#waterleigh"}>
+                <div className="transparent-background">
+                  <h3>Waterleigh</h3>
+                  <p>location here</p>
+                  <p>Size: values here</p>
+                </div>
+              </a>
             </div>
 
             <div className="nonapark-landing col-md-4">
-              <div className="transparent-background">
-                <h3>Nona Park</h3>
-                <p>location here</p>
-                <p>Project Type: type here</p>
-                <p>Size: values here</p>
-              </div>
+              <a href="#nonapark">
+                <div className="transparent-background">
+                  <h3>Nona Park</h3>
+                  <p>location here</p>
+                  <p>Size: values here</p>
+                </div>
+              </a>
             </div>
 
             <div className="seaisle-landing col-md-4">
-              <div className="transparent-background">
-                <h3>Sea Isle</h3>
-                <p>location here</p>
-                <p>Project Type: type here</p>
-                <p>Size: values here</p>
-              </div>
+              <a href="#seaisle">
+                <div className="transparent-background">
+                  <h3>Sea Isle</h3>
+                  <p>location here</p>
+                  <p>Size: values here</p>
+                </div>
+              </a>
             </div>
 
             <div className="altis-landing col-md-4">
-              <div className="transparent-background">
-                <h3>Altis at Sandlake</h3>
-                <p>location here</p>
-                <p>Project Type: type here</p>
-                <p>Size: values here</p>
-              </div>
+              <a href="#altis">
+                <div className="transparent-background">
+                  <h3>Altis at Sandlake</h3>
+                  <p>location here</p>
+                  <p>Size: values here</p>
+                </div>
+              </a>
             </div>
 
             <div className="dwell-landing col-md-4">
-              <div className="transparent-background">
-                <h3>Dwell at Nona Place</h3>
-                <p>location here</p>
-                <p>Project Type: type here</p>
-                <p>Size: values here</p>
-              </div>
+              <a href="#dwell">
+                <div className="transparent-background">
+                  <h3>Dwell at Nona Place</h3>
+                  <p>location here</p>
+                  <p>Size: values here</p>
+                </div>
+              </a>
             </div>
 
           </div>
