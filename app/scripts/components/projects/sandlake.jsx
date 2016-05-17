7f1431bbdd7a56de410d6fb3ed7df96c4b2bc410
Bruemor <arymer1@gmail.com>
2016-05-17 16:52:46 -0400

    changed the spacing of the image navigation.

--- app/scripts/components/projects/sandlake.jsx
+++ app/scripts/components/projects/sandlake.jsx
@@ -56,10 +56,10 @@ var SandlakeComponent = React.createClass({
             <div className="project-nav">
               <h2 className="altis-title">Altis at Sandlake</h2>
               <ul className="image-links">
-                <li className="image-list-item"><Link activeClass="active" className="image-two" to="image-two" spy={true} smooth={true} duration={500}></Link></li>
-                <li className="image-list-item"><Link activeClass="active" className="image-three" to="image-three" spy={true} smooth={true} duration={500}></Link></li>
-                <li className="image-list-item"><Link activeClass="active" className="image-four" to="image-four" spy={true} smooth={true} duration={500}></Link></li>
-                <li className="image-list-item"><Link activeClass="active" className="image-one" to="image-one" spy={true} smooth={true} duration={500}></Link></li>
+                <li className="altis-list-item"><Link activeClass="active" className="image-two" to="image-two" spy={true} smooth={true} duration={500}></Link></li>
+                <li className="altis-list-item"><Link activeClass="active" className="image-three" to="image-three" spy={true} smooth={true} duration={500}></Link></li>
+                <li className="altis-list-item"><Link activeClass="active" className="image-four" to="image-four" spy={true} smooth={true} duration={500}></Link></li>
+                <li className="altis-list-item"><Link activeClass="active" className="image-one" to="image-one" spy={true} smooth={true} duration={500}></Link></li>
                 <li><a onClick={() => scroll.scrollTo(100)}></a></li>
                 <li><a onClick={() => scroll.scrollMore(500)}></a></li>
               </ul>
