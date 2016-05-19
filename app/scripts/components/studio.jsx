706e0993516ff571121f2dff46b3409521398b60
Bruemor <arymer1@gmail.com>
2016-05-19 16:55:31 -0400

    created studio page component

---
+++ app/scripts/components/studio.jsx
@@ -0,0 +1,33 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('Backbone-React-Component');
+
+
+
+
+
+var StudioPageComponent = React.createClass({
+
+
+  render: function(){
+
+    return(
+      <div>
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+})
+
+
+
+
+//exports
+module.exports = {
+  'StudioPageComponent': StudioPageComponent
+}
