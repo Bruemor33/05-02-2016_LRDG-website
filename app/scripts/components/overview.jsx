4a0109ed81275ba44a57ca9e5ad6e7ca00808016
Bruemor <arymer1@gmail.com>
2016-05-19 16:55:08 -0400

    overview page component

---
+++ app/scripts/components/overview.jsx
@@ -0,0 +1,32 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('Backbone-React-Component');
+
+
+
+
+
+var OverviewComponent = React.createClass({
+
+
+  render: function(){
+
+    return(
+      <div>
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+});
+
+
+
+//exports
+module.exports = {
+  'OverviewComponent': OverviewComponent
+}
