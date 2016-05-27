09b6956b652fb53c6b4822c25141f9f9bbee227d
Bruemor <arymer1@gmail.com>
2016-05-27 09:19:08 -0400

    changed a classname from sea-isle-summary to the appropriate nona-park-summary to fix an enheritance issue on the sea-isle project page.

--- app/scripts/components/projects/nona-park.jsx
+++ app/scripts/components/projects/nona-park.jsx
@@ -17,7 +17,7 @@ var NonaParkComponent = React.createClass({
 
   handleClick: function(){
 
-    $('#sea-isle-summary').toggle('slow');
+    $('#nona-park-summary').toggle('slow');
 
   },
   componentDidMount: function(){
@@ -68,7 +68,7 @@ var NonaParkComponent = React.createClass({
             <p id="summary" onClick={this.handleClick} className="nona-click-event" role="button">
               Project Info<span className="glyphicon glyphicon-chevron-down" />
             </p>
-            <div id="sea-isle-summary" className="dropdown-menu">
+            <div id="nona-park-summary" className="dropdown-menu">
               <div className="project-type"><p><span>Project Type:</span>Multi-Family Community</p><br/></div>
               <div className="client-type"><p><span>Client:</span>Epoch Properties</p><br/></div>
               <div className="project-summary"><p>
