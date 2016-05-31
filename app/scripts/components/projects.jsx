062b38bb60ecfb2fa79b19998d66d394691ca7ca
Bruemor <arymer1@gmail.com>
2016-05-31 16:59:21 -0400

    tested some hover transition effects. Limited the selection down to the one the client likes. Still need to take the 180 flip out and do some more work getting the slide down perfect.

--- app/scripts/components/projects.jsx
+++ app/scripts/components/projects.jsx
@@ -12,28 +12,39 @@ var ProjectsComponent = React.createClass({
   render: function(){
 
     return(
-      <div className="page-background container">
+      <div className="container">
+        <div className="page-background"></div>
         <div className="row">
           <div className="project-one-container col-md-12">
 
             <div className="plaza-landing col-md-3">
-              <a href="#plazaUniversity">
-                <div className="transparent-background">
-                  <h3>Plaza at University</h3>
-                  <p>location here</p>
-                  <p>Size: values here</p>
-                </div>
-              </a>
+              <ul className="hover-effect-2 effect">
+                <a href="#plazaUniversity">
+                  <li className="transparent-background">
+                    <h3 className="zero">Plaza at University</h3>
+                    <p className="zero">location here</p>
+                    <p className="zero">Size: values here</p>
+                  </li>
+                  <li><img className="top" src="images/1-plaza.jpg" /></li>
+                </a>
+              </ul>
             </div>
 
             <div className="nonapark-landing col-md-3">
-              <a href="#nonapark">
-                <div className="transparent-background">
-                  <h3>Nona Park</h3>
-                  <p>location here</p>
-                  <p>Size: values here</p>
-                </div>
-              </a>
+              <ul className="hoover-effect">
+                <li>
+                  <figure>
+                    <a href="#nonapark">
+                      <img className="nonapark-thumbnail" src="images/5-nona-park-village.jpg"/>
+                      <figcaption className="transparent-background">
+                        <h3>Nona Park</h3>
+                        <p>location here</p>
+                        <p>Size: values here</p>
+                      </figcaption>
+                    </a>
+                  </figure>
+                </li>
+              </ul>
             </div>
 
             <div className="seaisle-landing col-md-3">
