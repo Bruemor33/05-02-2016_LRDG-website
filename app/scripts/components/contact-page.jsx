b092752960dc46c9ac5cf835738ea18be91f9d0e
Bruemor <arymer1@gmail.com>
2016-06-08 09:44:35 -0400

    added google map to contacts page

--- app/scripts/components/contact-page.jsx
+++ app/scripts/components/contact-page.jsx
@@ -21,15 +21,18 @@ var ContactPageComponent = React.createClass({
         <div className="contact-background">
           <div className="row">
             <div className="col-md-12">
-              <div className="company-container col-md-8">
+              <div className="company-container col-md-7">
                 <div className="company-logo"></div>
                 <h1 className="company-title">Land Resource Design Group</h1>
-                <p className="company-address">Address here</p>
-                <p className="company-emial">Query email here</p>
+                <p className="company-address">2929 Wadsworth Ave. 32806</p>
+                <p className="company-emial">info@lrdg-inc.com</p>
+                <ul>
+                  <li>social link</li>
+                  <li>social link</li>
+                </ul>
               </div>
-              <div className="google-map col-md-4">
-                <p>probably a map here.</p>
-                <p>link to directions</p>
+              <div className="google-map col-md-5">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.920700076393!2d-81.38118281018005!3d28.511031171583102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77b743865303b%3A0x35803a342f7b6c45!2s2929+Wadsworth+Ave%2C+Orlando%2C+FL+32806!5e0!3m2!1sen!2sus!4v1465219067841" width="400" height="300" frameborder="0" allowfullscreen></iframe>
               </div>
             </div>
           </div>
