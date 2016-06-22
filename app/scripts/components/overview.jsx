75b35c5488fb8c122f561d255c1cb28ddc41e262
Bruemor <arymer1@gmail.com>
2016-06-22 10:39:48 -0400

    brought in some filler data to start messing around with the layout design.

--- app/scripts/components/overview.jsx
+++ app/scripts/components/overview.jsx
@@ -30,15 +30,18 @@ var OverviewComponent = React.createClass({
               <div className="overview-aboutus">
                 <h5>intro statement</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col-md-12">
               <div className="overview-services">
-                <h5>intro statement</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h5>What We Do</h5>
+                <ul>
+                  <li><p>Land Planning</p></li>
+                  <li><p>Landscape Architecture</p></li>
+                  <li><p>Conceptual Render/Marketing graphics</p></li>
+                </ul>
               </div>
             </div>
           </div>
