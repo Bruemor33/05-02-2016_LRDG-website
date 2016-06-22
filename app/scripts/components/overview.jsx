1cf006cbcb2b28738234c9002d3b962deaa1b7f4
Bruemor <arymer1@gmail.com>
2016-06-22 15:27:43 -0400

    made some className changes to better replicate the design we are using in other parts of the site.

--- app/scripts/components/overview.jsx
+++ app/scripts/components/overview.jsx
@@ -15,10 +15,10 @@ var OverviewComponent = React.createClass({
   render: function(){
 
     return(
-      <div className="container">
-        <div className="page-background">
+      <div className="container-fluid">
+        <div className="practice-background">
           <div className="row">
-            <div className="col-md-12">
+            <div className="practice-container col-md-12">
               <div className="overview-title">
                 <div className="overview-logo"></div>
                 <h3>Land Resource Design Group</h3>
@@ -26,7 +26,7 @@ var OverviewComponent = React.createClass({
             </div>
           </div>
           <div className="row">
-            <div className="col-md-12">
+            <div className="statement-container col-md-12">
               <div className="overview-aboutus">
                 <h5>intro statement</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
