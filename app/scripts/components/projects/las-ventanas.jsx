1f7699effe0c9f209beb29e60acb1fac101511b1
Bruemor <arymer1@gmail.com>
2016-06-23 11:31:09 -0400

    edited component example to be specific to Las Ventanas. All functionally working correctly.

--- app/scripts/components/projects/las-ventanas.jsx
+++ app/scripts/components/projects/las-ventanas.jsx
@@ -18,7 +18,7 @@ var LasVentanasComponent = React.createClass({
 
   handleClick: function(){
 
-    $('#kindred-summary').toggle('slow');
+    $('#ventanas-summary').toggle('slow');
 
   },
   componentDidMount: function(){
@@ -52,36 +52,36 @@ var LasVentanasComponent = React.createClass({
 
     return(
       <div id="sea-isle-background" className="container-fluid">
-        <div classNme="col-md-12">
+        <div classNme="las-ventanas-container col-md-12">
           <div id="main-nav" className="writeup">
             <div className="project-nav">
-              <h2 className="kindred-title">Kindred</h2>
+              <h2 className="ventanas-title">Las Ventanas</h2>
               <ul className="image-links">
-                <li className="kindred-list-item"><Link activeClass="active" className="image-two" to="image-two" spy={true} smooth={true} duration={500}></Link></li>
-                <li className="kindred-list-item"><Link activeClass="active" className="image-three" to="image-three" spy={true} smooth={true} duration={500}></Link></li>
-                <li className="kindred-list-item"><Link activeClass="active" className="image-four" to="image-four" spy={true} smooth={true} duration={500}></Link></li>
-                <li className="kindred-list-item"><Link activeClass="active" className="image-one" to="image-one" spy={true} smooth={true} duration={500}></Link></li>
+                <li className="ventanas-list-item"><Link activeClass="active" className="ventanas-two" to="ventanas-two" spy={true} smooth={true} duration={500}></Link></li>
+                <li className="ventanas-list-item"><Link activeClass="active" className="ventanas-three" to="ventanas-three" spy={true} smooth={true} duration={500}></Link></li>
+                <li className="ventanas-list-item"><Link activeClass="active" className="ventanas-four" to="ventanas-four" spy={true} smooth={true} duration={500}></Link></li>
+                <li className="ventanas-list-item"><Link activeClass="active" className="ventanas-one" to="ventanas-one" spy={true} smooth={true} duration={500}></Link></li>
                 <li><a onClick={() => scroll.scrollTo(100)}></a></li>
                 <li><a onClick={() => scroll.scrollMore(500)}></a></li>
               </ul>
             </div>
-            <p>Osceola County, FL</p>
-            <p id="summary" onClick={this.handleClick} className="kindred-click-event" role="button">
+            <p>Bounton Beach, FL</p>
+            <p id="summary" onClick={this.handleClick} className="ventanas-click-event" role="button">
               Project Info<span className="glyphicon glyphicon-chevron-down" />
             </p>
-            <div id="kindred-summary" className="dropdown-menu">
-              <div className="project-type"><p><span>Project Type:</span>Single-Family Community Development</p><br/></div>
-              <div className="client-type"><p><span>Client:</span>DR Horton, Inc.</p><br/></div>
+            <div id="ventanas-summary" className="dropdown-menu">
+              <div className="project-type"><p><span>Project Type:</span>Mixed Use Development</p><br/></div>
+              <div className="client-type"><p><span>Client:</span>Epoch Residential</p><br/></div>
               <div className="project-summary"><p>
-                Located in Osceola County, FL, the Town of Kindred is a piece of land rich in local historic significance.  Once the site of the Partin Family Heart Bar Ranch, the Town of Kindred is a place where family, community and the great outdoors are the foundations of the “Kindred” quality of life.  Within Kindred, there’s access to Lake Tohopekaliga and multiple residential villages all integrated with sidewalks, trails, and various parks serving as common meeting places.  Kindred’s aesthetic provides comfortable warmth, stability, a variety of architectural design styles, and an abundance of healthy lifestyle amenities that are interconnected to encourage and celebrate community.
+                Las Ventanas is a Mediterranean style mix-use development located in Boynton Beach, FL.  It features two five-story towers, each with their own courtyard, a private parking garage, a variety of apartment styles and townhomes, retail shopping, restaurants, and professional offices.  Both courtyard designs were inspired by different interpretations of Las Ventanas, which is Spanish for “The Windows.”  One courtyard has curvilinear geometries implying movement through the window, while the other courtyard is beautifully formal suggesting views from the windows.  The design program included resort style swimming pools, spas, dining areas, lounge areas, outdoor grill areas, outdoor fire pits, vehicular entrance features, decorative paving, and signage.  Landscape, hardscape, lighting, and irrigation systems work in unison with the architecture to accentuate the narrative of the Mediterranean theme.
               </p></div>
             </div>
           </div>
         </div>
-        <div id="kindred-one"></div>
-        <div id="kindred-two"></div>
-        <div id="kindred-three"></div>
-        <div id="kindred-four"></div>
+        <div id="ventanas-one"></div>
+        <div id="ventanas-two"></div>
+        <div id="ventanas-three"></div>
+        <div id="ventanas-four"></div>
       </div>
     )
   }
