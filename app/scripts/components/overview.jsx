81f9d2ea3e66dff20e7f2d7185559710643c161f
Bruemor <arymer1@gmail.com>
2016-06-23 16:03:05 -0400

    shifted around the layout to have all the content in the same div to be used as a content scroll box.

--- app/scripts/components/overview.jsx
+++ app/scripts/components/overview.jsx
@@ -21,27 +21,37 @@ var OverviewComponent = React.createClass({
             <div className="practice-container col-md-12">
               <div className="overview-title">
                 <div className="overview-logo"></div>
-                <h3>Land Resource Design Group</h3>
+                <h2>Land Resource Design Group</h2>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="statement-container col-md-12">
-              <div className="overview-aboutus">
-                <h5>intro statement</h5>
+              <div className="scroller">
+                <h4>intro statement</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h4>What We Do</h4>
+                <ul>
+                  <li>
+                    <h4>Land Planning</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </li>
+                  <li>
+                    <h4>Landscape Architecture</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </li>
+                  <li>
+                    <h4>Conceptual Render/Marketing graphics</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col-md-12">
               <div className="overview-services">
-                <h5>What We Do</h5>
-                <ul>
-                  <li><p>Land Planning</p></li>
-                  <li><p>Landscape Architecture</p></li>
-                  <li><p>Conceptual Render/Marketing graphics</p></li>
-                </ul>
+
               </div>
             </div>
           </div>
