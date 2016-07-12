36cffabe9b9e94497eb7c7ca6df704231d44403c
Bruemor <arymer1@gmail.com>
2016-07-12 17:13:24 -0400

    made changes to the width of the contact info section, as well as changed font colors

--- app/scripts/components/contact-page.jsx
+++ app/scripts/components/contact-page.jsx
@@ -28,21 +28,22 @@ var ContactPageComponent = React.createClass({
                   </div>
                   <h1 className="company-title">Land Resource Design Group</h1>
                 </div>
-              </div>
-              <div className="company-info">
-                <div className="social-links">
-                  <p className="social-links-caption">Find us online!</p>
-                  <ul>
-                    <li>
-                      <a href="https://www.facebook.com/Land-Resource-Design-Group-872873609490285/?fref=ts">
-                        <i className="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li><i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></li>
-                  </ul>
-                </div>
-                <div className="google-map">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.920700076393!2d-81.38118281018005!3d28.511031171583102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77b743865303b%3A0x35803a342f7b6c45!2s2929+Wadsworth+Ave%2C+Orlando%2C+FL+32806!5e0!3m2!1sen!2sus!4v1465219067841" width="600" height="300" frameborder="0" allowfullscreen></iframe>
+
+                <div className="company-info">
+                  <div className="social-links">
+                    <p className="social-links-caption">Find us online!</p>
+                    <ul>
+                      <li>
+                        <a href="https://www.facebook.com/Land-Resource-Design-Group-872873609490285/?fref=ts">
+                          <i className="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                      <li><i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></li>
+                    </ul>
+                  </div>
+                  <div className="google-map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.920700076393!2d-81.38118281018005!3d28.511031171583102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77b743865303b%3A0x35803a342f7b6c45!2s2929+Wadsworth+Ave%2C+Orlando%2C+FL+32806!5e0!3m2!1sen!2sus!4v1465219067841" width="460" height="300" frameborder="0" allowfullscreen></iframe>
+                  </div>
                 </div>
               </div>
             </div>
