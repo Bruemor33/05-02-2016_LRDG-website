83fa1efc013f3b31bf01ebc8889cc80e76cd8950
Bruemor <arymer1@gmail.com>
2016-07-14 15:50:45 -0400

    starting putting some layout into this component.

--- app/scripts/components/studio.jsx
+++ app/scripts/components/studio.jsx
@@ -15,8 +15,23 @@ var StudioPageComponent = React.createClass({
   render: function(){
 
     return(
-      <div>
-        <div>
+      <div className="container">
+        <div className="col-md-6">
+          <div className="row">
+            <ul>
+              <li className="barry">
+
+              </li>
+              <li className="jim">
+
+              </li>
+              <li className="scott">
+
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-md-6">
 
         </div>
       </div>
