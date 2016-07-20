0b791cc8a6bc86f10fb07243ea0cefc66c594047
Bruemor <arymer1@gmail.com>
2016-07-20 09:28:02 -0400

    began building out the studio page

--- app/scripts/components/studio.jsx
+++ app/scripts/components/studio.jsx
@@ -18,17 +18,22 @@ var StudioPageComponent = React.createClass({
       <div className="container">
         <div className="col-md-6">
           <div className="row">
-            <ul>
-              <li className="barry">
-
-              </li>
-              <li className="jim">
-
-              </li>
-              <li className="scott">
-
-              </li>
-            </ul>
+            <div className="barry col-md-12">
+              <img></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="jim col-md-12">
+              <img></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="scott col-md-12">
+              <img></img>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
           </div>
         </div>
         <div className="col-md-6">
