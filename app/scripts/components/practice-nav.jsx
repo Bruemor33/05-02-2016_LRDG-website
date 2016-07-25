06e84529a1598696fcd490db3fd90c19aaabf7ba
Bruemor <arymer1@gmail.com>
2016-07-25 16:54:46 -0400

    created alternate navigation component in order to give the user an idea of what page they are on.

---
+++ app/scripts/components/practice-nav.jsx
@@ -0,0 +1,34 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('Backbone-React-Component');
+
+
+var PracticeNavigation = React.createClass({
+
+  render: function(){
+
+    return(
+      <div className="container-fluid">
+        <nav id="site-nav" className="row">
+          <div className="projects-navigation col-md-12">
+            <img src="./images/small-lrdg-logo.png" className="landing-logo" />
+            <a href="#overview" className="projects-overview">Practice</a>
+            <a href="#projects" className="projects-projects">Projects</a>
+            <a href="#people" className="projects-people">Studio</a>
+            <a href="#contact" className="projects-contact">Contact</a>
+            <a href="#" className="landing-company-name">LRDG: Practice</a>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+})
+
+
+//Component Export
+module.exports = {
+  'PracticeNavigation': PracticeNavigation
+}
---
+++ app/scripts/components/studio-nav.jsx
@@ -0,0 +1,34 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('Backbone-React-Component');
+
+
+var StudioPageNavigation = React.createClass({
+
+  render: function(){
+
+    return(
+      <div className="container-fluid">
+        <nav id="site-nav" className="row">
+          <div className="projects-navigation col-md-12">
+            <img src="./images/small-lrdg-logo.png" className="landing-logo" />
+            <a href="#overview" className="projects-overview">Practice</a>
+            <a href="#projects" className="projects-projects">Projects</a>
+            <a href="#people" className="projects-people">Studio</a>
+            <a href="#contact" className="projects-contact">Contact</a>
+            <a href="#" className="landing-company-name">LRDG: Studio</a>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+})
+
+
+//Component Export
+module.exports = {
+  'StudioPageNavigation': StudioPageNavigation
+}
