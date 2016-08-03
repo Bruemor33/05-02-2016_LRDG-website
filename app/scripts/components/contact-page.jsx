2af7b3053b2c1522799397774ab4357595a098d3
Bruemor <arymer1@gmail.com>
2016-08-03 14:33:36 -0400

    made some changes to the map size, text on this page, as well as layout of the information. Also brougt in an emial link.

--- app/scripts/components/contact-page.jsx
+++ app/scripts/components/contact-page.jsx
@@ -31,9 +31,11 @@ var ContactPageComponent = React.createClass({
 
                 <div className="company-info">
                   <div className="social-links">
-                    <p className="social-links-caption">Find us online!</p>
-                    <ul>
-                      <li>
+                    <p className="social-links-caption">To find out more, contact us at:</p>
+                    <div className="info-emial"><a href="mailto:info@lrdg-inc.com">info@lrdg-inc.com</a></div>
+                    <p className="social-media-container">Also check us out on social media for any news or job opportunities</p>
+                    <ul className="social-media-links">
+                      <li className="facebook-spacing">
                         <a href="https://www.facebook.com/Land-Resource-Design-Group-872873609490285/?fref=ts">
                           <i className="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
                         </a>
@@ -42,7 +44,7 @@ var ContactPageComponent = React.createClass({
                     </ul>
                   </div>
                   <div className="google-map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.920700076393!2d-81.38118281018005!3d28.511031171583102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77b743865303b%3A0x35803a342f7b6c45!2s2929+Wadsworth+Ave%2C+Orlando%2C+FL+32806!5e0!3m2!1sen!2sus!4v1465219067841" width="460" height="300" frameborder="0" allowfullscreen></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.920700076393!2d-81.38118281018005!3d28.511031171583102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77b743865303b%3A0x35803a342f7b6c45!2s2929+Wadsworth+Ave%2C+Orlando%2C+FL+32806!5e0!3m2!1sen!2sus!4v1465219067841" width="460" height="230" frameborder="0" allowfullscreen></iframe>
                   </div>
                 </div>
               </div>
