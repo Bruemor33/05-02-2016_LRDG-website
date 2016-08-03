a89bc4b72068cce66a7dfc46d4e54eeeee75770b
Bruemor <arymer1@gmail.com>
2016-08-03 14:35:17 -0400

    finalized the layout and functionality of the modals.

--- app/scripts/components/studio.jsx
+++ app/scripts/components/studio.jsx
@@ -3,11 +3,24 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Modal = require('boron/OutlineModal');
+var Modal = require('boron/FlyModal');
 require('Backbone-React-Component');
 
 
 
+
+
+var modalStyle = {
+  width: '1020px'
+}
+
+var backdropStyle = {
+  backgroundColor: 'rgba(0, 0, 0, 0.6)'
+}
+
+var contentStyle = {
+  backgroundColor: 'rgba(255, 255, 255, 0.9)'
+}
 
 
 var StudioPageComponent = React.createClass({
@@ -52,58 +65,75 @@ var StudioPageComponent = React.createClass({
       <div className="container-fluid">
         <div className="studio-background"></div>
         <div className="row">
+          <div className="practice-container col-md-12">
+            <div className="overview-title">
+              <div className="overview-logo"></div>
+              <h2>Studio Leads</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
           <div className="studio-content-container col-md-12">
             <div className="barry col-md-4">
-              <div onClick={this.barryModal} className="toggle-icon">
-                <i className="fa fa-caret-square-o-down fa-2x" aria-hidden="true"></i>
+              <div className="barry-title">
+                <h5>Barry Rymer, PLA</h5>
               </div>
-              <div className="barry-photo"></div>
-              <Modal ref="barry" className="barry-writeup">
-                <h4 className="staff-title">Barry H. Rymer, PLA</h4>
+              <div className="barry-photo" onClick={this.barryModal}></div>
+              <Modal ref="barry" modalStyle={modalStyle} backdropStyle={backdropStyle} contentStyle={contentStyle}>
+                <div className="modal-picture-barry" onClick={this.hideBarry}></div>
+                <div className="barry-writeup" onClick={this.hideBarry}>
+                  <h4 className="staff-title">Barry H. Rymer, PLA</h4>
 
-                <p className="barry-first-paragraph">Barry began his landscape architecture career over 30 years ago in Central Florida. Having received his BLA from the University of Florida in 1985, he sought after a fully integrated understanding of the profession and spent much of his career working for leading multi-disciplinary design firms. It was with these experiences where he learned the value of a coordinated design effort with all design disciplines in developing buildable and lasting solutions.</p>
+                  <p className="barry-first-paragraph">Barry began his landscape architecture career over 30 years ago in Central Florida. Having received his BLA from the University of Florida in 1985, he sought after a fully integrated understanding of the profession and spent much of his career working for leading multi-disciplinary design firms. It was with these experiences where he learned the value of a coordinated design effort with all design disciplines in developing buildable and lasting solutions.</p>
 
-                <p className="barry-second-paragraph">In the spring of 2011, Barry founded Land Resource Design Group. The establishment of the firm was inspired by his core belief of viewing land as an extremely valuable resource. Barry works closely with his clients and leadership team to collaborate with other design disciplines in providing thoughtful design, carefully crafted execution, and timeless value in the communities they create. Over the years, he has had the privilege to work with award-wining master-planned community developers, leading national builders, industry-leading multi-family developers and world leaders in theme park entertainment.</p>
+                  <p className="barry-second-paragraph">In the spring of 2011, Barry founded Land Resource Design Group. The establishment of the firm was inspired by his core belief of viewing land as an extremely valuable resource. Barry works closely with his clients and leadership team to collaborate with other design disciplines in providing thoughtful design, carefully crafted execution, and timeless value in the communities they create. Over the years, he has had the privilege to work with award-wining master-planned community developers, leading national builders, industry-leading multi-family developers and world leaders in theme park entertainment.</p>
 
-                <p className="barry-third-paragraph">Barry loves his role as husband and father to his wife of thirty years and his three talented and now adult children. In his spare time, he is a big sports fan, enjoys managing his daughter’s golf career and serving up some tasty BBQ from his prized Big Green Egg.</p>
-                <div onClick={this.hideBarry}>
-                  <i className="fa fa-times" aria-hidden="true"></i>
+                  <p className="barry-third-paragraph">Barry loves his role as husband and father to his wife of thirty years and his three talented and now adult children. In his spare time, he is a big sports fan, enjoys managing his daughter’s golf career and serving up some tasty BBQ from his prized Big Green Egg.</p>
+                  <div className="modal-close" onClick={this.hideBarry}>
+                    <i className="fa fa-times" aria-hidden="true"></i>
+                  </div>
                 </div>
               </Modal>
             </div>
             <div className="scott col-md-4">
-              <div onClick={this.scottModal} className="toggle-icon">
-                <i className="fa fa-caret-square-o-down fa-2x" aria-hidden="true"></i>
+              <div className="scott-title">
+                <h5>Scott Moorman, Sr. Planner/Project Manager</h5>
               </div>
-              <div className="scott-photo"></div>
-              <Modal ref="scott" className="scott-writeup">
-                <h4 className="staff-title">Scott Moorman, Sr. Planner/Project Manager</h4>
+              <div className="scott-photo" onClick={this.scottModal}></div>
+              <Modal ref="scott" modalStyle={modalStyle} modalStyle={modalStyle} backdropStyle={backdropStyle} contentStyle={contentStyle}>
+                <div className="modal-picture-scott" onClick={this.hideScott}></div>
+                <div className="scott-writeup" onClick={this.hideScott}>
+                  <h4 className="staff-title">Scott Moorman, Sr. Planner/Project Manager</h4>
 
-                <p className="scott-first-paragraph">Since Scott graduated from the University of Florida’s school of Landscape Architecture in 1993, he has compiled over 20 years of experience practicing both Landscape Architect and Land Planning in the Central Florida arena. </p>
+                  <p className="scott-first-paragraph">Since Scott graduated from the University of Florida’s school of Landscape Architecture in 1993, he has compiled over 20 years of experience practicing both Landscape Architect and Land Planning in the Central Florida arena. </p>
 
-                <p className="scott-second-paragraph">Prior to joining LRDG, he worked at several multi-disciplinary Civil and Architectural firms and has developed a strong working knowledge of each facet of the design, entitlements, production and construction processes.  His experience spans nearly all levels of project styles that a Landscape Architect could hope to work on during their career.  DRI development, site planning and entitlements, rezonings and comp plan amendments, preliminary engineering and the full gamut of Landscape Architectural services for both private and public sector Clients. </p>
+                  <p className="scott-second-paragraph">Prior to joining LRDG, he worked at several multi-disciplinary Civil and Architectural firms and has developed a strong working knowledge of each facet of the design, entitlements, production and construction processes.  His experience spans nearly all levels of project styles that a Landscape Architect could hope to work on during their career.  DRI development, site planning and entitlements, rezonings and comp plan amendments, preliminary engineering and the full gamut of Landscape Architectural services for both private and public sector Clients. </p>
 
-                <p className="scott-third-paragraph">Here at LRDG, Scott specializes in design and construction plans preparation, project management, consultant team coordination, plans processing, government approvals, construction administration and services marketing to our Client base.</p>
+                  <p className="scott-third-paragraph">Here at LRDG, Scott specializes in design and construction plans preparation, project management, consultant team coordination, plans processing, government approvals, construction administration and services marketing to our Client base.</p>
 
-                <p className="scott-fourth-paragraph">Away from the office Scott is an avid Outdoorsman and enjoys spending time with his fiancé and adult son hunting, fishing and participating in shooting sports.</p>
-                <div onClick={this.hideScott}>
-                  <i className="fa fa-times" aria-hidden="true"></i>
+                  <p className="scott-fourth-paragraph">Away from the office Scott is an avid Outdoorsman and enjoys spending time with his fiancé and adult son hunting, fishing and participating in shooting sports.</p>
+                  <div className="modal-close" onClick={this.hideScott}>
+                    <i className="fa fa-times" aria-hidden="true"></i>
+                  </div>
                 </div>
               </Modal>
             </div>
             <div className="jim col-md-4">
-              <div onClick={this.jimModal} className="toggle-icon">
-                <i className="fa fa-caret-square-o-down fa-2x" aria-hidden="true"></i>
+              <div className="jim-title">
+                <h5>Jim Morgan, PLA</h5>
               </div>
-              <div className="jim-photo"></div>
-              <Modal ref="jim" className="jim-writeup">
-                <h4 className="staff-title">Jim Morgan, PLA</h4>
+              <div className="jim-photo" onClick={this.jimModal}></div>
+              <Modal ref="jim" modalStyle={modalStyle} backdropStyle={backdropStyle} contentStyle={contentStyle}>
+                <div className="modal-picture-jim" onClick={this.hideJim}></div>
+                <div className="jim-writeup" onClick={this.hideJim}>
+                  <h4 className="staff-title">Jim Morgan, PLA</h4>
 
-                <p className="jim-first-paragraph">Jim has actively practiced in the central Florida area since 2005 with over 10 years of experience in landscape architecture.  He’s gained a broad knowledge, which allows him to focus on many issues of every project.  As a landscape architect, his responsibilities are diverse and include client development, contract administration, project management, and production of all documents that leave the office.  His involvement in every project, which generally includes strategic planning, project themes, and overall document production lends a broad perspective to all levels of the development of the project.</p>
+                  <p className="jim-first-paragraph">Jim has actively practiced in the central Florida area since 2005 with over 10 years of experience in landscape architecture.  He’s gained a broad knowledge, which allows him to focus on many issues of every project.  As a landscape architect, his responsibilities are diverse and include client development, contract administration, project management, and production of all documents that leave the office.  His involvement in every project, which generally includes strategic planning, project themes, and overall document production lends a broad perspective to all levels of the development of the project.</p>
 
-                <p className="jim-second-paragraph">In his spare time, Jim enjoys writing and illustrating children’s books inspired by his 2 kids and wife of 10 years.</p>
-                <div onClick={this.hideJim}>
-                  <i className="fa fa-times" aria-hidden="true"></i>
+                  <p className="jim-second-paragraph">In his spare time, Jim enjoys writing and illustrating children’s books inspired by his 2 kids and wife of 10 years.</p>
+                  <div className="jim-modal-close" onClick={this.hideJim}>
+                    <i className="fa fa-times" aria-hidden="true"></i>
+                  </div>
                 </div>
               </Modal>
             </div>
