6ad117c7e7f90f028f0195100d044abd56a1a3ee
Bruemor <arymer1@gmail.com>
2016-08-24 11:49:59 -0400

    created client listing page navigation

---
+++ app/scripts/components/clients-nav.jsx
@@ -0,0 +1,37 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('Backbone-React-Component');
+
+
+var ClientsPageNavigation = React.createClass({
+
+  render: function(){
+
+    return(
+      <div className="container-fluid">
+        <nav id="site-nav" className="row">
+          <div className="projects-navigation col-md-12">
+            <a href="#"><img src="./images/small-lrdg-logo.png" className="projects-logo" /></a>
+            <a href="#overview" className="projects-overview">Practice</a>
+            <a href="#projects" className="projects-projects">Projects</a>
+            <a href="#people" className="projects-people">Studio</a>
+            <a href="#clients" id="clients-active" className="projects-clients">Clients</a>
+            <a href="#contact" className="projects-contact">Contact</a>
+            <a href="#" className="navigation-home">
+              <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+            </a>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+})
+
+
+//Component Export
+module.exports = {
+  'ClientsPageNavigation': ClientsPageNavigation
+}
