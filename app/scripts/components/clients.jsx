f1f0da4eb2fdb66a48ff4522f98b69a7800c5057
Bruemor <arymer1@gmail.com>
2016-09-01 11:07:31 -0400

    made a few className changes.

--- app/scripts/components/clients.jsx
+++ app/scripts/components/clients.jsx
@@ -18,37 +18,101 @@ var ClientsComponent = React.createClass({
       <div className="container-fluid">
         <div className="practice-background">
           <div className="row">
-            <div className="practice-container col-md-12">
-              <div className="overview-title">
+            <div className="clients-container col-md-12">
+              <div className="clients-title">
                 <div className="overview-logo"></div>
-                <h2>Overview</h2>
+                <h2>Client Listing</h2>
               </div>
             </div>
           </div>
           <div className="row">
-            <div className="statement-container col-md-12">
-              <div className="scroller">
-                <p>Land Resource Design Group, aka LRDG, was founded by long time Central Florida landscape architect Barry H. Rymer.  Mr. Rymer has practiced land planning and landscape architecture in the Central Florida and throughout the southeast United States for over 30 years.</p>
-                <p>LRDG was formed with the philosophy of viewing land as a valuable resource for man's sustainability through improving our quality of life, environmental stewardship and economic prosperity. LRDG practices this philosophy through the planning, design and implementation of new communities and the enhancement of existing communities.</p>
-                <p>LRDG's professional staff have over 60 years of Central Florida planning and landscape architecture experience in providing services in the following private and public markets.</p>
-                <h4>What We Do</h4>
-                <ul className="practice-services">
-                  <li className="services-link-container">
-                    <a href="#overview/community">Community Planning</a>
-                  </li>
-                  <li className="services-link-container">
-                    <a href="#overview/landscape-architecture">Landscape Architecture</a>
-                  </li>
-                  <li className="services-link-container">
-                    <a href="#overview/construction-administration">Construction Administration</a>
-                  </li>
-                  <li className="services-link-container">
-                    <a href="#overview/graphics">Graphics</a>
-                  </li>
-                </ul>
+            <div className="container-cancel col-md-12">
+              <div className="client-statement-container col-md-9">
+                <div className="scroller">
+                  <p>The Following is a sampling of the CLients that LRDG's leadership team has had the privilege to work with during our combined 60+ years of experience in Central Florida</p>
+
+                  <ul className="client-listing col-md-12">
+                    <div className="block-one col-md-4">
+                      <li>
+                        <h5 className="client-listing-catagory">Master-Planned Communities:</h5>
+                        <ul className="client-names">
+                          <li><p>DR Horton</p></li>
+                          <li><p>Harmony Development</p></li>
+                          <li><p>Harmony on Lake Eloise, LLC</p></li>
+                          <li><p>The Ginn Company</p></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <h5 className="client-listing-catagory">Community Development Districts:</h5>
+                        <ul className="client-names">
+                          <li><p>Anthem Park CDD</p></li>
+                          <li><p>Concorde Estates CDD</p></li>
+                          <li><p>Harmony CDD</p></li>
+                          <li><p>Pine Island CDD</p></li>
+                        </ul>
+                      </li>
+                    </div>
+
+                    <div className="block-two col-md-4">
+                      <li>
+                        <h5 className="client-listing-catagory">Residential Communities:</h5>
+                        <ul className="client-names">
+                          <li><p>DR Horton</p></li>
+                          <li><p>Hanover Capitol</p></li>
+                          <li><p>KB Homes</p></li>
+                          <li><p>Pulte Homes</p></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <h5 className="client-listing-catagory">Resort/Entertainment:</h5>
+                        <ul className="client-names">
+                          <li><p>Disney Development Company</p></li>
+                          <li><p>Godwin's Gatorland</p></li>
+                          <li><p>Timescape Resorts</p></li>
+                          <li><p>Walt Disney Imagineering</p></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <h5 className="client-listing-catagory">Educational:</h5>
+                        <ul className="client-names">
+                          <li><p>UCF Convovation Corp.</p></li>
+                        </ul>
+                      </li>
+                    </div>
+
+                    <div className="block-three col-md-4">
+                      <li>
+                        <h5 className="client-listing-catagory">Multi-Family Communities:</h5>
+                        <ul className="client-names">
+                          <li><p>Altman Development Company</p></li>
+                          <li><p>Crescent Resources</p></li>
+                          <li><p>Epock Residential</p></li>
+                          <li><p>The Klein Company</p></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <h5 className="client-listing-catagory">Sr. Adult Communities:</h5>
+                        <ul className="client-names">
+                          <li><p>Pulte - Del Webb</p></li>
+                          <li><p>Senior LIving Communities</p></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <h5 className="client-listing-catagory">Student Housing - Mixed Use:</h5>
+                        <ul className="client-names">
+                          <li><p>American Campus Communities</p></li>
+                        </ul>
+                      </li>
+                    </div>
+                  </ul>
+                </div>
+              </div>
+              <div className="clients-image-container col-md-3">
+                <div className="clients-image"></div>
               </div>
             </div>
           </div>
+
         </div>
       </div>
     )
