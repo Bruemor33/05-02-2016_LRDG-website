4ee4bd884f0277aae5aa44a560b560951b7d99ff
Bruemor <arymer1@gmail.com>
2016-09-07 14:27:59 -0400

    update syntax due to a warning that may be breaking npm run build and deploy

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -10,102 +10,102 @@ require('Backbone-React-Component')
 
 var Router = Backbone.Router.extend({
   routes: {
-    "": "index",
-    "projects": "projects",
-    "kindred": "kindred",
-    "waterleigh": "waterleigh",
-    "nonapark": "nonapark",
-    "seaisle": "seaisle",
-    "altis": "altis",
-    "dwell": "dwell",
-    "plazaUniversity": "plazaUniversity",
-    "ventanas": "ventanas",
-    "brisas": "brisas",
-    "harmony": "harmony",
-    "magic": "magic",
-    "bella": "bella",
-    "overview": "overview",
-    "overview/community": "community",
-    "overview/landscape-architecture": "landscape",
-    "overview/construction-administration": "construction",
-    "overview/graphics": "graphics",
-    "people": "people",
-    "clients": "clients",
-    "contact": "contact",
-    "images": "images",
-    "*notfound": "notfound"
+    '': 'index',
+    'projects': 'projects',
+    'kindred': 'kindred',
+    'waterleigh': 'waterleigh',
+    'nonapark': 'nonapark',
+    'seaisle': 'seaisle',
+    'altis': 'altis',
+    'dwell': 'dwell',
+    'plazaUniversity': 'plazaUniversity',
+    'ventanas': 'ventanas',
+    'brisas': 'brisas',
+    'harmony': 'harmony',
+    'magic': 'magic',
+    'bella': 'bella',
+    'overview': 'overview',
+    'overview/community': 'community',
+    'overview/landscape-architecture': 'landscape',
+    'overview/construction-administration': 'construction',
+    'overview/graphics': 'graphics',
+    'people': 'people',
+    'clients': 'clients',
+    'contact': 'contact',
+    'images': 'images',
+    '*notfound': 'notfound'
   },
   initialize: function(){
     //set up heroku app for LRDG images
   },
   index: function(){
-    this.current = "index"
+    this.current = 'index'
   },
   projects: function(){
-    this.current = "projects"
+    this.current = 'projects'
   },
   kindred: function(){
-    this.current = "kindred"
+    this.current = 'kindred'
   },
   waterleigh: function(){
-    this.current = "waterleigh"
+    this.current = 'waterleigh'
   },
   nonapark: function(){
-    this.current = "nonapark"
+    this.current = 'nonapark'
   },
   seaisle: function(){
-    this.current = "seaisle"
+    this.current = 'seaisle'
   },
   altis: function(){
-    this.current = "altis"
+    this.current = 'altis'
   },
   dwell: function(){
-    this.current = "dwell"
+    this.current = 'dwell'
   },
   plazaUniversity: function(){
-    this.current = "plazaUniversity"
+    this.current = 'plazaUniversity'
   },
   ventanas: function(){
-    this.current = "ventanas"
+    this.current = 'ventanas'
   },
   brisas: function(){
-    this.current = "brisas"
+    this.current = 'brisas'
   },
   harmony: function(){
-    this.current = "harmony"
+    this.current = 'harmony'
   },
   magic: function(){
-    this.current = "magic"
+    this.current = 'magic'
   },
   bella: function(){
-    this.current = "bella"
+    this.current = 'bella'
   },
   overview: function(){
-    this.current = "overview"
+    this.current = 'overview'
   },
   community: function(){
-    this.current = "overview/community"
+    this.current = 'overview/community'
   },
   landscape: function(){
-    this.current = "overview/landscape-architecture"
+    this.current = 'overview/landscape-architecture'
   },
   construction: function(){
-    this.current = "overview/construction-administration"
+    this.current = 'overview/construction-administration'
   },
   graphics: function(){
-    this.current = "overview/graphics"
+    this.current = 'overview/graphics'
   },
   people: function(){
-    this.current = "people"
+    this.current = 'people'
   },
   clients: function(){
-    this.current = "clients"
+    this.current = 'clients'
   },
   contact: function(){
-    this.current = "contact"
+    this.current = 'contact'
   },
   images: function(){
-    this.current = "images"
+    this.current = 'images'
   }
 });
 
