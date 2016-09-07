69eeb1cf9861f6c3604672fdbca838c854a31fa6
Bruemor <arymer1@gmail.com>
2016-09-07 15:15:11 -0400

    added ; to where the warnings in npm run build where directing to.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -5,7 +5,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 // var Parse = require('parse');
 // var ParseReact = require('parse-react');
-require('Backbone-React-Component')
+require('Backbone-React-Component');
 
 
 var Router = Backbone.Router.extend({
@@ -39,73 +39,73 @@ var Router = Backbone.Router.extend({
     //set up heroku app for LRDG images
   },
   index: function(){
-    this.current = 'index'
+    this.current = 'index';
   },
   projects: function(){
-    this.current = 'projects'
+    this.current = 'projects';
   },
   kindred: function(){
-    this.current = 'kindred'
+    this.current = 'kindred';
   },
   waterleigh: function(){
-    this.current = 'waterleigh'
+    this.current = 'waterleigh';
   },
   nonapark: function(){
-    this.current = 'nonapark'
+    this.current = 'nonapark';
   },
   seaisle: function(){
-    this.current = 'seaisle'
+    this.current = 'seaisle';
   },
   altis: function(){
-    this.current = 'altis'
+    this.current = 'altis';
   },
   dwell: function(){
-    this.current = 'dwell'
+    this.current = 'dwell';
   },
   plazaUniversity: function(){
-    this.current = 'plazaUniversity'
+    this.current = 'plazaUniversity';
   },
   ventanas: function(){
-    this.current = 'ventanas'
+    this.current = 'ventanas';
   },
   brisas: function(){
-    this.current = 'brisas'
+    this.current = 'brisas';
   },
   harmony: function(){
-    this.current = 'harmony'
+    this.current = 'harmony';
   },
   magic: function(){
-    this.current = 'magic'
+    this.current = 'magic';
   },
   bella: function(){
-    this.current = 'bella'
+    this.current = 'bella';
   },
   overview: function(){
-    this.current = 'overview'
+    this.current = 'overview';
   },
   community: function(){
-    this.current = 'overview/community'
+    this.current = 'overview/community';
   },
   landscape: function(){
-    this.current = 'overview/landscape-architecture'
+    this.current = 'overview/landscape-architecture';
   },
   construction: function(){
-    this.current = 'overview/construction-administration'
+    this.current = 'overview/construction-administration';
   },
   graphics: function(){
-    this.current = 'overview/graphics'
+    this.current = 'overview/graphics';
   },
   people: function(){
-    this.current = 'people'
+    this.current = 'people';
   },
   clients: function(){
-    this.current = 'clients'
+    this.current = 'clients';
   },
   contact: function(){
-    this.current = 'contact'
+    this.current = 'contact';
   },
   images: function(){
-    this.current = 'images'
+    this.current = 'images';
   }
 });
 
