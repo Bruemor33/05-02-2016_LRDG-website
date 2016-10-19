8c120492d54d4f42bc47cd1beb24fb184fc71cac
Bruemor <arymer1@gmail.com>
2016-10-19 09:29:06 -0400

    made the navigation into a modal

--- app/scripts/components/mobile-nav.jsx
+++ app/scripts/components/mobile-nav.jsx
@@ -3,18 +3,55 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Modal = require('boron/FadeModal');
 require('Backbone-React-Component');
 
 
 
+var modalStyle = {
+  width: '315px'
+}
+
+var backdropStyle = {
+  backgroundColor: 'rgba(0, 0, 0, 0.6)'
+}
+
+var contentStyle = {
+  backgroundColor: 'rgba(255, 255, 255, 0.9)'
+}
 
 
 var MobileNavigation = React.createClass({
+
+  showModal: function() {
+    this.refs.modal.show();
+  },
+
+  hideModal: function() {
+    this.refs.modal.hide();
+  },
+
+  callback: function(event){
+    console.log(event);
+  },
+
+  navModal: function() {
+    this.refs.nav.show();
+  },
+
+  hideNav: function() {
+    this.refs.nav.hide();
+  },
+
 
   handleClick: function() {
 
     $('.mobile-nav-items').toggle('slow');
 
+  },
+
+  hideClick: function() {
+    $('.mobile-nav-items').toggle('slow');
   },
 
 
@@ -23,23 +60,25 @@ var MobileNavigation = React.createClass({
     return(
       <div className="container-fluid">
         <div id="mobile-nav">
-          <div className="mobile-nav-toggle">
-            <i onClick={this.handleClick} className="fa fa-bars fa-2x" aria-hidden="true"></i>
+          <div className="mobile-nav-toggle" onClick={this.navModal} >
+            <i className="fa fa-bars fa-2x" aria-hidden="true"></i>
           </div>
+          <Modal ref="nav" modalStyle={modalStyle} backdropStyle={backdropStyle} contentStyle={contentStyle}>
             <ul className="mobile-nav-items">
               <li className="mobile-nav-links">
-                <a href="#overview" className="mobile-practice">Practice</a>
+                <a href="#overview" className="mobile-practice" onClick={this.hideNav}>Practice</a>
               </li>
               <li className="mobile-nav-links">
-                <a href="#projects" className="mobile-projects">Projects</a>
+                <a href="#projects" className="mobile-projects" onClick={this.hideNav}>Projects</a>
               </li>
               <li className="mobile-nav-links">
-                <a href="#people" className="mobile-studio">Studio</a>
+                <a href="#people" className="mobile-studio" onClick={this.hideNav}>Studio</a>
               </li>
               <li className="mobile-nav-links">
-                <a href="#contact" className="mobile-contact">Contact</a>
+                <a href="#contact" className="mobile-contact" onClick={this.hideNav}>Contact</a>
               </li>
             </ul>
+          </Modal>
         </div>
       </div>
     )
