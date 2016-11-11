584e4b580007a836e94c930ddc6900c0eb800a7b
Bruemor <arymer1@gmail.com>
2016-11-11 09:21:32 -0500

    fixed layout issue that was causing issues with out image container

--- app/scripts/components/community-planning.jsx
+++ app/scripts/components/community-planning.jsx
@@ -26,61 +26,59 @@ var CommunityPlanningComponent = React.createClass({
                 </div>
               </div>
             </div>
-            <div className="content-scroller">
-              <div className="row">
-                <div className="community-statement-container col-md-12">
-                  <div className="community-scroller">
-                    <p>Community planning is the initial and most critical opportunity to create value through resource-based land planning. This allows the establishment of optimal land use classifications, site-specific design criteria, and marketable densities. Combined, these assist in creating an architectural canvas that define successful and viable “outdoor living spaces” throughout the community.</p>
-                    <p>Our community planning develops the value of the land through the skillful execution of the following services:</p>
-                    <ul>
-                      <li>
-                        <p>Projects Programming</p>
-                      </li>
-                      <li>
-                        <p>Site Analysis and Due Diligence Research</p>
-                      </li>
-                      <li>
-                        <p>Land Planning</p>
-                      </li>
-                      <li>
-                        <p>Golf Course Routing</p>
-                      </li>
-                      <li>
-                        <p>Permitting Coordination</p>
-                      </li>
-                      <li>
-                        <p>Consultant Team Leader Coordination</p>
-                      </li>
-                      <li>
-                        <p>Design Process Coordination</p>
-                      </li>
-                      <li>
-                        <p>Rezoning and Comprehensive Plan Amendment Exhibit Preparation</p>
-                      </li>
-                    </ul>
-                  </div>
+            <div className="row">
+              <div className="community-statement-container col-md-12">
+                <div className="community-scroller">
+                  <p>Community planning is the initial and most critical opportunity to create value through resource-based land planning. This allows the establishment of optimal land use classifications, site-specific design criteria, and marketable densities. Combined, these assist in creating an architectural canvas that define successful and viable “outdoor living spaces” throughout the community.</p>
+                  <p>Our community planning develops the value of the land through the skillful execution of the following services:</p>
+                  <ul>
+                    <li>
+                      <p>Projects Programming</p>
+                    </li>
+                    <li>
+                      <p>Site Analysis and Due Diligence Research</p>
+                    </li>
+                    <li>
+                      <p>Land Planning</p>
+                    </li>
+                    <li>
+                      <p>Golf Course Routing</p>
+                    </li>
+                    <li>
+                      <p>Permitting Coordination</p>
+                    </li>
+                    <li>
+                      <p>Consultant Team Leader Coordination</p>
+                    </li>
+                    <li>
+                      <p>Design Process Coordination</p>
+                    </li>
+                    <li>
+                      <p>Rezoning and Comprehensive Plan Amendment Exhibit Preparation</p>
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div className="construction-images-container col-md-7">
-                <div className="row">
-                  <div className="construction-row-one col-md-12">
-                    <div className="image-one-spacing col-md-6">
-                      <div className="community-image-one"></div>
-                    </div>
-                    <div className="image-two-spacing col-md-6">
-                      <div className="community-image-two"></div>
-                    </div>
-                  </div>
+            </div>
+          </div>
+          <div className="community-images-container col-md-7">
+            <div className="row">
+              <div className="construction-row-one col-md-12">
+                <div className="image-one-spacing col-md-6">
+                  <div className="community-image-one"></div>
                 </div>
-                <div className="row">
-                  <div className="construction-row-two col-md-12">
-                    <div className="image-three-spacing col-md-6">
-                      <div className="community-image-three"></div>
-                    </div>
-                    <div className="image-four-spacing col-md-6">
-                      <div className="community-image-four"></div>
-                    </div>
-                  </div>
+                <div className="image-two-spacing col-md-6">
+                  <div className="community-image-two"></div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="construction-row-two col-md-12">
+                <div className="image-three-spacing col-md-6">
+                  <div className="community-image-three"></div>
+                </div>
+                <div className="image-four-spacing col-md-6">
+                  <div className="community-image-four"></div>
                 </div>
               </div>
             </div>
