fe94a883da9ea3f97f78c964e0da33c49558a20a
Bruemor <arymer1@gmail.com>
2016-11-17 14:27:43 -0500

    made a modal change

--- app/scripts/components/studio.jsx
+++ app/scripts/components/studio.jsx
@@ -8,8 +8,6 @@ require('Backbone-React-Component');
 
 
 
-
-
 var modalStyle = {
   width: '1020px'
 }
@@ -66,13 +64,13 @@ var StudioPageComponent = React.createClass({
         <div className="studio-background"></div>
         <div className="row">
           <div className="practice-container col-md-12">
-            <div className="studio-title">
+            <div className="overview-title">
               <div className="overview-logo"></div>
               <h2>Studio Leaders</h2>
             </div>
           </div>
         </div>
-        <div className="content-row row">
+        <div className="row">
           <div className="studio-content-container col-md-12">
             <div className="barry col-md-4">
               <div className="barry-photo" role="button" onClick={this.barryModal}></div>
