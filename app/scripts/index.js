2ed47b38eadf264b5e021662807ad79289d9a88d
Bruemor <arymer1@gmail.com>
2016-11-17 14:28:02 -0500

    added a NODE_ENV conditional check

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -12,6 +12,10 @@ var router = require('./router-interface/router.js');
 var InterfaceComponent = require('./router-interface/interface-controller.jsx').InterfaceComponent;
 
 
+if (process.env.NODE_ENV === 'production'){
+  console.log('production only');
+}
+
 $(function(){
   ReactDOM.render(
     React.createElement(InterfaceComponent, {
